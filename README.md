326493aebc31bc4925aeb1aa1f61cd5080f45494
SkyblockTools <lucas.j.tan@gmail.com>
2021-10-17 21:57:38 -0700

    update database

--- README.md
+++ README.md
@@ -1,14 +1,25 @@
 # Key Value Store For Sensor Data from S2
 
-To Set Key Value: 
-1. /set/|key|/|value|
+**GET**
+Endpoint: `/get/<key>`
+Returns the value associated with the key given.
+If there is no key given, returns the whole json object
+If the key doesn't exist, returns 404
 
-
-To Get Key Value:
-1. /get/|key|
-
-Replace anything within || including |
-
-
-To Deploy:
-	1. git push heroku main 
+**POST**
+Endpoint: `/set/`
+Use a json object with the fields that need to be updated.
+ex (javascript):
+```
+let json = {"atmospheric_temp":8};
+let xhr = new XMLHttpRequest();
+xhr.onload = function(e) {
+	console.log(xhr.responseText)
+	...
+}
+r.open("POST", url);
+r.setRequestHeader('Content-Type', 'application/json');
+r.send(JSON.stringify(json));
+```
+# Deployment
+``git push heroku main``
--- database.py
+++ database.py
@@ -1,31 +1,52 @@
-from flask import Flask, redirect, url_for, render_template, request, jsonify
+from flask import Flask, redirect, url_for, render_template, request, jsonify,abort
+from flask_cors import CORS
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
 import numpy as np
 import cv2
 import base64
 import threading
 
 app = Flask(__name__)
+cors = CORS(app, resources={r"/*": {"origins": "*"}})
 temp_image = cv2.resize(cv2.imread("plant_demo.JPG"), (360, 360))
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 data = {"atmospheric_temp": 30, "reservoir_temp": 25, "foliage_feed": base64.b64encode(cv2.imencode(".JPG", temp_image)[1]).decode("utf-8")}
-# data = {"atmospheric_temp": 30, "reservoir_temp": 25, "foliage_feed": 3}
 lock = threading.Lock()
-@app.route("/", methods = ['POST', 'GET'])
-def home():
-    return jsonify(data)
+limiter = Limiter(
+    app,
+    key_func=get_remote_address,
+    default_limits=["300 per day", "30 per minute"]
+)
 
+ 
+@app.route('/', methods=['GET'])
+def home():
+	return ""
+
+@app.route('/get/', methods=['GET'])
+def get():
+	with lock:
+		return jsonify(data)
 
 @app.route('/get/<key>', methods=['GET'])
-def get(key):
-    with lock:
-        return data[key]
+def getkey(key):
+	with lock:
+		if key in data:
+			return str(data[key])
+		else:
+			abort(404)
 
-@app.route('/set/<key>/<val>', methods=['GET', 'POST'])
-def set(key, val):
-    with lock:
-        data[key]=val
-        return redirect(url_for('home'))
+@app.route('/set/', methods=['POST'])
+def set():
+	with lock:
+		content = request.get_json()
+		for key, value in content.items():
+			if key in data:
+				print("set " + key + " to " + str(value))
+				data[key] = value
+		return redirect(url_for('home'))
 
 if __name__ == "__main__":
     app.run()
-
+	
