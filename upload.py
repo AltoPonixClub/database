54e03f95aee5f6d20a9be270311f6b6d9c56e7d3
aoberai <aoberai05@gmail.com>
2021-10-24 13:07:14 -0700

    5p by 5p 10fps

--- upload.py
+++ upload.py
@@ -2,11 +2,12 @@ import base64
 import cv2
 import json
 import requests
+import time
 
 cap = cv2.VideoCapture(0)
 url = "https://altoponix-database.herokuapp.com/set"
 headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
-
+counter = 0
 while True:
     data = {
         "key": "672ef79b4d0a4805bc529d1ae44bc26b",
@@ -15,7 +16,8 @@ while True:
                 ".JPG",
                 cv2.resize(
                     cap.read()[1],
-                    (480,
-                     360)))[1]).decode("utf-8"),
+                    (10,10)))[1]).decode("utf-8"),
         "atmospheric_temp": 8}
     requests.post(url, data=json.dumps(data), headers=headers)
+    time.sleep(0.1)
+    print(counter := counter + 1)
