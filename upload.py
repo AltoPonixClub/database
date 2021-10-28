347a9c4f07468b7ce92fc4284649bccc1031bd5b
aoberai <aoberai05@gmail.com>
2021-10-27 19:01:07 -0700

    compression qual

--- upload.py
+++ upload.py
@@ -4,23 +4,24 @@ import json
 import requests
 import time
 
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture("../../python/blind-navigation/TurnSidewalk.mp4")
 url = "https://altoponix-database.herokuapp.com/set"
 headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
 counter = 0
 while True:
     raw_image = cap.read()[1]
-    size = (raw_image.shape[0]//19, raw_image.shape[1]//19)
+    size = (raw_image.shape[0]//30, raw_image.shape[1]//30)
     print(size)
     data = {
         "key": "672ef79b4d0a4805bc529d1ae44bc26b",
         "foliage_feed": base64.b64encode(
             cv2.imencode(
-                ".JPG",
+                ".JPEG",
                 cv2.resize(
                     raw_image,
-                    size))[1]).decode("utf-8"),
+                    size), [int(cv2.IMWRITE_JPEG_QUALITY), 95])[1]).decode("utf-8"),
         "atmospheric_temp": 8}
+    # print(len(data["foliage_feed"]))
     requests.post(url, data=json.dumps(data), headers=headers)
     time.sleep(0.1)
     print(counter := counter + 1)
