b3bf82160dfa75cd9f745451d67a6c1abfe7cc2d
SkyblockTools <lucas.j.tan@gmail.com>
2021-10-28 19:46:58 -0700

    completely rewrite sql

--- main.py
+++ main.py
@@ -18,44 +18,87 @@ limiter = Limiter(
     default_limits=["300 per minute"]
 )
 
-
 def setup_app(app):
     with sqlite3.connect(database_path) as conn:
         utils.init(conn)
 
-
 setup_app(app)
 
-
-@app.route('/get/', methods=['GET'])
-def get():
+@app.route('/api/v1/monitors/get', methods=['GET'])
+def get_monitor():
     with sqlite3.connect(database_path) as conn:
-        key = request.args.get('key')
-        j = utils.get(conn, key)
+        monitor_id = request.args.get('monitor_id')
+        j = utils.get_monitors(conn, monitor_id)
         if j == {}:
-            return {"success": False, "cause": "Invalid key"}, 400
+            return {"success": False, "cause": "Invalid monitor_id"}, 400
         return {"success": True, "data": j}
 
 
-@app.route('/set/', methods=['POST'])
-def update():
+@app.route('/api/v1/monitors/update', methods=['POST'])
+def update_monitor():
     with sqlite3.connect(database_path) as conn:
         content = utils.isolate_updatable(request.get_json())
-        if "key" not in content:
-            return {"success": False,
-                    "cause": "Missing one or more fields: [key]"}, 400
+        if "id" not in content or content["id"] == None:
+            return {"success": False, "cause": "Missing one or more fields: [id]"}, 400
         if len(content) < 2:
             return {"success": False, "cause": "Couldn't update any fields"}, 400
-        d = utils.update(conn, content)
+        d = utils.update_monitor(conn, content)
         if d is not None:
             return d
         return redirect(url_for('home'))
 
 
+@app.route('/api/v1/monitors/add', methods=['POST'])
+def add_monitor():
+    with sqlite3.connect(database_path) as conn:
+        content = request.get_json()
+        if "user_id" not in content or content["user_id"] == None:
+            return {"success": False,
+                    "cause": "Missing one or more fields: [user_id]"}, 400
+        if "monitor_id" not in content or content["monitor_id"] == None:
+            return {"success": False,
+                    "cause": "Missing one or more fields: [monitor_id]"}, 400
+        d = utils.add_monitor(conn, (content["monitor_id"],content["user_id"]))
+        if d is not None:
+            return d
+        return redirect(url_for('home'))
+
+@app.route('/api/v1/monitors/reset', methods=['POST'])
+def reset_monitor():
+    with sqlite3.connect(database_path) as conn:
+        content = request.get_json()
+        if "monitor_id" not in content or content["monitor_id"] == None:
+            return {"success": False,
+                    "cause": "Missing one or more fields: [monitor_id]"}, 400
+        d = utils.reset_monitor(conn,content["monitor_id"])
+        if d is not None:
+            return d
+        return redirect(url_for('home'))
+
+@app.route('/api/v1/monitors/delete', methods=['POST'])
+def delete_monitor():
+    with sqlite3.connect(database_path) as conn:
+        content = request.get_json()
+        if "monitor_id" not in content or content["monitor_id"] == None:
+            return {"success": False,
+                    "cause": "Missing one or more fields: [monitor_id]"}, 400
+        d = utils.delete_monitor(conn,content["monitor_id"])
+        if d is not None:
+            return d
+        return redirect(url_for('home'))
+
+@app.route('/api/v1/users/monitors/get', methods=['GET'])
+def get_user():
+    with sqlite3.connect(database_path) as conn:
+        user_id = request.args.get('user_id')
+        j = utils.get_users(conn, user_id)
+        if j == {}:
+            return {"success": False, "cause": "Invalid user_id"}, 400
+        return {"success": True, "data": j}
+
 @app.route('/', methods=['GET'])
 def home():
     return ""
-
 
 @app.errorhandler(404)
 def fallback(_):
--- utils.py
+++ utils.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import json
+import time
 
 table_typings = None
 
@@ -8,17 +10,32 @@ table_typings = None
 # ? params automatically escape inputs
 # https://stackoverflow.com/questions/29528511/python-sqlite3-sql-injection-vulnerable-code
 
+def current_milli_time():
+    return round(time.time() * 1000)
 
 def init(conn):
     conn.executescript('''
         CREATE TABLE if not exists monitors (
-            key char(32) PRIMARY KEY NOT NULL,
-            atmospheric_temp DECIMAL(10,5),
-            reservoir_temp DECIMAL(10,5),
-            foliage_feed text
-            CHECK(length(key) == 32 and TYPEOF(key) == 'text' and (TYPEOF(atmospheric_temp) == 'real' or TYPEOF(atmospheric_temp) == 'integer' or TYPEOF(atmospheric_temp) == 'null') and (TYPEOF(reservoir_temp) == 'real' or TYPEOF(reservoir_temp) == 'integer' or TYPEOF(reservoir_temp) == 'null') and (TYPEOF(foliage_feed) == 'text' or TYPEOF(foliage_feed) == 'null'))
+            id char(32) PRIMARY KEY NOT NULL,
+            atmospheric_temp TEXT,
+            reservoir_temp TEXT,
+            light_intensity TEXT,
+            soil_moisture TEXT,
+            electrical_conductivity TEXT,
+            ph TEXT,
+            dissolved_oxygen TEXT,
+            air_flow TEXT,
+            foliage_feed TEXT,
+            root_stream TEXT,
+            CHECK(length(id) == 32 and TYPEOF(id) == 'text')
         );
-
+    ''')
+    conn.executescript('''
+        CREATE TABLE if not exists users (
+            monitor_id char(32) PRIMARY KEY NOT NULL,
+            user_id char(32) NOT NULL,
+            CHECK(length(user_id) == 32 and TYPEOF(user_id) == 'text' and length(monitor_id) == 32 and TYPEOF(monitor_id) == 'text')
+        );
     ''')
     cur = conn.cursor()
     cur.execute("PRAGMA table_info(monitors)")
@@ -33,25 +50,39 @@ def insert(conn, val):
 
 
 def isolate_updatable(content):
-    return {i[0]: i[1] for i in content.items(
-    ) if i[0] in table_typings and i[1] is not None}
+    return {i[0]: i[1] for i in content.items() if i[0] in table_typings and i[1] is not None}
 
-
-def update(conn, val):
+def update_monitor(conn, val):
+    s = current_milli_time()
+    j = get_monitors(conn,val["id"])
+    l = []
+    for i in j.items():
+        if i[0] in val and i[0] != "id":
+            if type(i[1]) is dict and type(val[i[0]]) is int or type(val[i[0]]) is float:
+                try:
+                    i[1]["value"] = float(val[i[0]])
+                    i[1]["history"][str(s)] = float(val[i[0]])
+                    l.append((i[0],json.dumps(i[1])))
+                except:
+                    pass
+            if type(i[1]) is str:
+                l.append((i[0],val[i[0]]))
+    if len(l) == 0:
+        return {"success": False, "cause": "Invalid data"}, 400
     cur = conn.cursor()
     sql = "UPDATE monitors SET "
     data = []
-    for i in val.items():
-        if i[0] != "key":
+    for i in l:
+        if i[0] != "id":
             sql += i[0] + " = ?,"
             data.append(i[1])
-    sql = sql[:-1] + " WHERE key = ?"
-    data.append(val["key"])
+    sql = sql[:-1] + " WHERE id = ?"
+    data.append(val["id"])
     try:
         cur.execute(sql, tuple(data))
         conn.commit()
-    except BaseException:
-        return {"success": False, "cause": "Invalid data!"}, 400
+    except:
+        return {"success": False, "cause": "Invalid data"}, 400
 
 
 def to_string(conn):
@@ -63,8 +94,7 @@ def sqltojson(s):
     for a in s:
         a = list(a)
         k = a.pop(0)
-        t = ({table_typings[i + 1]: a[i]
-             for i in range(len(table_typings) - 1)})
+        t = ({table_typings[i + 1]: optionaljson(a[i]) for i in range(len(table_typings) - 1)})
         if len(s) == 1:
             return t
         else:
@@ -72,10 +102,98 @@ def sqltojson(s):
     return json
 
 
-def get(conn, key=None):
+def get_monitors(conn, key=None):
     cur = conn.cursor()
     if key is not None:
-        cur.execute("SELECT * FROM monitors WHERE key = ?", (key,))
+        cur.execute("SELECT * FROM monitors WHERE id = ?", (key,))
     else:
         cur.execute("SELECT * FROM monitors")
-    return sqltojson(cur.fetchall())
+    v = cur.fetchall()
+    return sqltojson(v)
+
+def get_users(conn, key=None):
+    cur = conn.cursor()
+    if key is not None:
+        cur.execute("SELECT monitors.id FROM users JOIN monitors ON monitors.id = users.monitor_id WHERE user_id = ?", (key,))
+        v = cur.fetchall()
+        return [x[0] for x in v]
+    else:
+        cur.execute("SELECT * FROM users")
+        v = cur.fetchall()
+        s = {}
+        for a in v:
+            if a[1] not in s:
+                s[a[1]] = []
+            s[a[1]].append(a[0])
+        return s
+    
+
+def add_monitor(conn, val):
+    cur = conn.cursor()
+    try:
+        cur.execute("INSERT INTO users VALUES (?, ?)", val)
+        cur.execute("""INSERT INTO monitors VALUES (?,
+            '{"value":null,"history":{}}',
+            '{"value":null,"history":{}}',
+            '{"value":null,"history":{}}',
+            '{"value":null,"history":{}}',
+            '{"value":null,"history":{}}',
+            '{"value":null,"history":{}}',
+            '{"value":null,"history":{}}',
+            '{"value":null,"history":{}}',
+            '',
+            ''
+        )
+        """, (val[0],))
+        conn.commit()
+    except Exception as e:
+        if str(e).startswith("CHECK constraint failed"):
+            return {"success": False, "cause": "Invalid id"}, 400
+        if str(e).startswith("UNIQUE constraint failed"):
+            return {"success": False, "cause": "monitor_id already exists"}, 400
+        return {"success": False, "cause": "Unknown Error"}, 400
+
+def reset_monitor(conn, id):
+    if len(get_monitors(conn, id)) == 0:
+        return {"success": False, "cause": "Invalid id"}, 400
+    cur = conn.cursor()
+    try:
+        cur.execute("""UPDATE monitors SET 
+            atmospheric_temp = '{"value":null,"history":{}}',
+            reservoir_temp = '{"value":null,"history":{}}',
+            light_intensity = '{"value":null,"history":{}}',
+            soil_moisture = '{"value":null,"history":{}}',
+            electrical_conductivity = '{"value":null,"history":{}}',
+            ph = '{"value":null,"history":{}}',
+            dissolved_oxygen = '{"value":null,"history":{}}',
+            air_flow = '{"value":null,"history":{}}',
+            foliage_feed = '',
+            root_stream = ''
+            WHERE id = ?
+        """, (id,))
+        conn.commit()
+    except Exception as e:
+        if str(e).startswith("CHECK constraint failed"):
+            return {"success": False, "cause": "Invalid id"}, 400
+        if str(e).startswith("UNIQUE constraint failed"):
+            return {"success": False, "cause": "monitor_id already exists"}, 400
+        return {"success": False, "cause": "Unknown Error"}, 400
+
+def delete_monitor(conn, id):
+    if len(get_monitors(conn, id)) == 0:
+        return {"success": False, "cause": "Invalid id"}, 400
+    cur = conn.cursor()
+    try:
+        cur.execute("DELETE FROM users WHERE monitor_id = ?", (id,))
+        cur.execute("DELETE FROM monitors WHERE id = ?", (id,))
+        conn.commit()
+    except Exception as e:
+        print(e)
+        return {"success": False, "cause": "Unknown Error"}, 400
+    
+
+def optionaljson(v):
+    try:
+        return json.loads(v)
+    except:
+        return v
