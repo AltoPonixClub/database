821f3ae2d471de4299ab0d034fcad7bdc4433d58
aoberai <aoberai05@gmail.com>
2021-11-25 10:02:43 -0800

    fixed ffmpeg command, need to find viewer to check working

--- streaming/run.sh
+++ streaming/run.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# vuquangtrong.github.io
 
 # create a folder in shared memory
 mkdir -p /dev/shm/streaming
@@ -12,24 +11,36 @@ else
 fi
 
 # create video segments for HLS and DASH
-ffmpeg -y \
-    -input_format h264 \
-    -f video4linux2 \
-    -framerate 25 \
-    -use_wallclock_as_timestamps 1 \
-    -i 0 \
-    -c:v copy \
-    -f dash \
-    -ldash 1 \
-    -seg_duration 1 \
-    -frag_duration 1 \
-    -streaming 1 \
-    -window_size 30 -remove_at_exit 1 \
-    -strict experimental -lhls 1 \
-    -hls_playlist 1 -hls_master_name live.m3u8 \
-    -utc_timing_url https://time.akamai.com/?iso \
-    -write_prft 1 \
-    -target_latency 1 \
-    /dev/shm/streaming/live.mpd &
-    
+# ffmpeg -y \
+#     -input_format h264 \
+#     -f video4linux2 \
+#     -framerate 25 \
+#     -use_wallclock_as_timestamps 1 \
+#     -i 0 \
+#     -c:v copy \
+#     -f dash \
+#     -ldash 1 \
+#     -seg_duration 1 \
+#     -frag_duration 1 \
+#     -streaming 1 \
+#     -window_size 30 -remove_at_exit 1 \
+#     -strict experimental -lhls 1 \
+#     -hls_playlist 1 -hls_master_name live.m3u8 \
+#     -utc_timing_url https://time.akamai.com/?iso \
+#     -write_prft 1 \
+#     -target_latency 1 \
+#     /dev/shm/streaming/live.mpd &
+   
+ffmpeg \
+    -f v4l2 -video_size 640x480 \
+    -i /dev/video0 \
+    -c:v libx264 -crf 21 -preset veryfast \
+    -b:v 100M -b:a 128k \
+    -f hls \
+    -hls_list_size 2 \
+    -hls_flags independent_segments -hls_flags delete_segments \
+    -hls_segment_type mpegts \
+    -hls_segment_filename data%02d.ts \
+    -master_pl_name master.m3u8 out1
+
 python3 server.py
