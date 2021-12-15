6f19d1da3e0f45eefdcfb22f5eb4b5203e128287
SkyblockTools <lucas.j.tan@gmail.com>
2021-12-14 17:17:22 -0800

    Add comment s

--- main.py
+++ main.py
@@ -12,6 +12,8 @@ import time
 
 database_path = "./database.db"
 vid_path_prefix = "./assets"
+
+# Initialize Flask Application
 app = Flask(__name__)
 socketio = SocketIO(app, cors_allowed_origins="*", async_mode='threading')
 app.config['JSON_SORT_KEYS'] = False
@@ -19,124 +21,135 @@ app.config['CORS_HEADERS'] = 'Content-Type'
 cors = CORS(app, resources={r"/*": {"origins": "*"}})
 lock = threading.Lock()
 limiter = Limiter(
-    app,
-    key_func=get_remote_address,
-    default_limits=["300 per minute"]
+  app,
+  key_func=get_remote_address,
+  default_limits=["300 per minute"]
 )
 
+# Socket Variables
 clients = {}
 devices = {}
 monitordevice = {}
 
+# Initialize Database
 def setup_app(app):
-    with sqlite3.connect(database_path) as conn:
-        utils.init(conn)
+  with sqlite3.connect(database_path) as conn:
+    utils.init(conn)
 
 setup_app(app)
 
+# Get Fragment Video 
+# will be deprecated once sftp starts working
 @app.route('/api/v1/assets/<frag>', methods=['GET'])
 def get_frag(frag):
-    try:
-        with open(os.path.join(vid_path_prefix, frag), 'rb') as f:
-            return f.read()
-    except Exception as e:
-        return "Frag Doesn't Exist"
-
-    # return str(sorted(os.listdir(vid_path_prefix)))
-
-@app.route('/api/v1/monitors/get', methods=['GET'])
-def get_monitor():
-    with sqlite3.connect(database_path) as conn:
-        monitor_id = request.args.get('monitor_id')
-        j = utils.get_monitors(conn, monitor_id)
-        if j == {}:
-            return {"success": False, "cause": "Invalid monitor_id"}, 400
-        return {"success": True, "data": j}
+  try:
+    with open(os.path.join(vid_path_prefix, frag), 'rb') as f:
+      return f.read()
+  except Exception as e:
+    return "Frag Doesn't Exist"
+  # return str(sorted(os.listdir(vid_path_prefix)))
 
 def fetch_frag(content):
-    feed = content["foliage_feed"]
-    vid_name = "frag%s.mp4" % round(time.time())
-    frag_path = os.path.join(vid_path_prefix, vid_name)
-    with open(frag_path, "wb") as f:
-        vid = bytes.fromhex(feed)
-        f.write(vid)
-    for path in sorted([path for path in os.listdir(vid_path_prefix) if '.mp4' in path])[:-2]:
-        os.remove(os.path.join(vid_path_prefix, path))
-    return vid_name
+  feed = content["foliage_feed"]
+  vid_name = "frag%s.mp4" % round(time.time())
+  frag_path = os.path.join(vid_path_prefix, vid_name)
+  with open(frag_path, "wb") as f:
+    vid = bytes.fromhex(feed)
+    f.write(vid)
+  for path in sorted([path for path in os.listdir(vid_path_prefix) if '.mp4' in path])[:-2]:
+    os.remove(os.path.join(vid_path_prefix, path))
+  return vid_name
 
+# Database Methods
+@app.route('/api/v1/monitors/get', methods=['GET'])
+def get_monitor():
+  with sqlite3.connect(database_path) as conn:
+    monitor_id = request.args.get('monitor_id')
+    data = utils.get_monitors(conn, monitor_id)
+    if data == {}:
+      return {"success": False, "cause": "Invalid monitor_id"}, 400
+    return {"success": True, "data": data}
 
 @app.route('/api/v1/monitors/update', methods=['POST'])
 def update_monitor():
-    with sqlite3.connect(database_path) as conn:
-        content = utils.isolate_updatable(request.get_json())
-        content["foliage_feed"] = fetch_frag(content) # Key to access vid on filesystem
-        if "id" not in content or content["id"] is None:
-            return {"success": False,
-                    "cause": "Missing one or more fields: [id]"}, 400
-        if len(content) < 2:
-            return {"success": False, "cause": "Couldn't update any fields"}, 400
-        d = utils.update_monitor(conn, content)
-        if d is not None:
-            return d
-        return redirect(url_for('home'))
+  with sqlite3.connect(database_path) as conn:
+    content = utils.isolate_updatable(request.get_json())
+    content["foliage_feed"] = fetch_frag(content) # Key to access vid on filesystem
+    if "id" not in content or content["id"] is None:
+      return {"success": False,
+              "cause": "Missing one or more fields: [id]"}, 400
+    if len(content) < 2:
+      return {"success": False, "cause": "Couldn't update any fields"}, 400
+    data = utils.update_monitor(conn, content)
+    if data is not None:
+      return data
+    return redirect(url_for('home'))
 
 
 @app.route('/api/v1/monitors/add', methods=['POST'])
 def add_monitor():
-    with sqlite3.connect(database_path) as conn:
-        content = request.get_json()
-        if "user_id" not in content or content["user_id"] is None:
-            return {"success": False,
-                    "cause": "Missing one or more fields: [user_id]"}, 400
-        if "monitor_id" not in content or content["monitor_id"] is None:
-            return {"success": False,
-                    "cause": "Missing one or more fields: [monitor_id]"}, 400
-        d = utils.add_monitor(
-            conn, (content["monitor_id"], content["user_id"]))
-        if d is not None:
-            return d
-        return redirect(url_for('home'))
+  with sqlite3.connect(database_path) as conn:
+    content = request.get_json()
+    if "user_id" not in content or content["user_id"] is None:
+      return {"success": False,
+              "cause": "Missing one or more fields: [user_id]"}, 400
+    if "monitor_id" not in content or content["monitor_id"] is None:
+      return {"success": False,
+              "cause": "Missing one or more fields: [monitor_id]"}, 400
+    d = utils.add_monitor(
+      conn, (content["monitor_id"], content["user_id"]))
+    if d is not None:
+      return d
+    return redirect(url_for('home'))
 
 
 @app.route('/api/v1/monitors/reset', methods=['POST'])
 def reset_monitor():
-    with sqlite3.connect(database_path) as conn:
-        content = request.get_json()
-        if "monitor_id" not in content or content["monitor_id"] is None:
-            return {"success": False,
-                    "cause": "Missing one or more fields: [monitor_id]"}, 400
-        d = utils.reset_monitor(conn, content["monitor_id"])
-        if d is not None:
-            return d
-        return redirect(url_for('home'))
+  with sqlite3.connect(database_path) as conn:
+    content = request.get_json()
+    if "monitor_id" not in content or content["monitor_id"] is None:
+      return {"success": False,
+              "cause": "Missing one or more fields: [monitor_id]"}, 400
+    d = utils.reset_monitor(conn, content["monitor_id"])
+    if d is not None:
+        return d
+    return redirect(url_for('home'))
 
 
 @app.route('/api/v1/monitors/delete', methods=['POST'])
 def delete_monitor():
-    with sqlite3.connect(database_path) as conn:
-        content = request.get_json()
-        if "monitor_id" not in content or content["monitor_id"] is None:
-            return {"success": False,
-                    "cause": "Missing one or more fields: [monitor_id]"}, 400
-        d = utils.delete_monitor(conn, content["monitor_id"])
-        if d is not None:
-            return d
-        return redirect(url_for('home'))
+  with sqlite3.connect(database_path) as conn:
+    content = request.get_json()
+    if "monitor_id" not in content or content["monitor_id"] is None:
+      return {"success": False,
+              "cause": "Missing one or more fields: [monitor_id]"}, 400
+    d = utils.delete_monitor(conn, content["monitor_id"])
+    if d is not None:
+      return d
+    return redirect(url_for('home'))
 
 
 @app.route('/api/v1/owners/get', methods=['GET'])
 def get_owners():
-    with sqlite3.connect(database_path) as conn:
-        user_id = request.args.get('user_id')
-        j = utils.get_owners(conn, user_id)
-        if j == {}:
-            return {"success": False, "cause": "Invalid user_id"}, 400
-        return {"success": True, "data": j}
+  with sqlite3.connect(database_path) as conn:
+    user_id = request.args.get('user_id')
+    j = utils.get_owners(conn, user_id)
+    if j == {}:
+      return {"success": False, "cause": "Invalid user_id"}, 400
+    return {"success": True, "data": j}
 
 @app.route('/', methods=['GET'])
 def home():
   return 200
 
+# Socket Handling
+
+# query: event sent by client to start sending commands.
+# JSON data:
+# {
+#    "monitor_id": string, the monitor_id to start sending to (REQUIRED)
+# }
+# emits "query ok" if connection succeeded, "query failed" if connection didn't (and closes the connection)
 @socketio.on('query')                    
 def query(content):      
   if "monitor_id" not in content or content["monitor_id"] is None or type(content["monitor_id"]) != str or len(content["monitor_id"]) != 32:
@@ -145,6 +158,13 @@ def query(content):
   clients[request.sid] = content["monitor_id"]
   emit('query ok', {'reason': 'Query ok'}, room=request.sid) 
 
+# con: event sent by device to start listening commands.
+# JSON data:
+# {
+#    "monitor_id": string, the monitor_id of this device (REQUIRED)
+# }
+# the monitor_id given must be a unique one that is currently not connected to a websocket
+# emits "con ok" if connection succeeded, "con failed" if connection didn't (and closes the connection)
 @socketio.on('con')                    
 def query(content):      
   if "monitor_id" not in content or content["monitor_id"] is None or type(content["monitor_id"]) != str or len(content["monitor_id"]) != 32:
@@ -157,6 +177,14 @@ def query(content):
   monitordevice[content["monitor_id"]] = True
   emit('con ok', {'reason': 'Connection ok'}, room=request.sid) 
 
+# command send: event sent by client to send a command to the device.
+# JSON data:
+# {
+#    "command": string, the command to send (REQUIRED)
+# }
+# emits "command failed" to the client if the command was invalid
+# emits "command ok" to the client
+# emits "command do" to the device with the same json data given
 @socketio.on('command send')                    
 def send(content):      
   if "command" not in content or content["command"] is None or type(content["command"]) != str or len(content["command"]) == 0:
@@ -168,19 +196,27 @@ def send(content):
         emit('command do', {'command': content["command"]}, room=sid) 
   emit('command ok', {'reason': 'Command ok'}, room=request.sid) 
 
-
+# command done: event sent by device to signal when a command finishes
+# emits command finish to the client
 @socketio.on('command done')                    
 def done(content):
   for sid, m in clients.items():
     if m == devices[request.sid]:
       emit('command finish', {'reason': 'Command finish'}, room=sid) 
 
+# command error: event sent by device to signal when a command fails
+# JSON data:
+# {
+#    "reason": string, the reason to send
+# }
+# emits command failed to the client
 @socketio.on('command error')                    
 def done(content):
   for sid, m in clients.items():
     if m == clients[request.sid]:
       emit('command failed', {'reason': content.reason}, room=sid) 
 
+# Remove devices
 @socketio.on('disconnect')  
 def handle_disc():
   if request.sid in devices:
@@ -189,12 +225,13 @@ def handle_disc():
   clients.pop(request.sid, None)
   devices.pop(request.sid, None)
 
+# 404 handler
 @app.errorhandler(404)
 def fallback(_):
   return {"success": False,
           "cause": "The specified URI was not found on this server"}, 404
 
-
+# run the app
 if __name__ == "__main__":
    socketio.run(app)
 
--- utils.py
+++ utils.py
@@ -12,175 +12,170 @@ table_typings = None
 
 
 def current_milli_time():
-    return round(time.time() * 1000)
-
+  return round(time.time() * 1000)
 
 def init(conn):
-    conn.executescript('''
-        CREATE TABLE if not exists monitors (
-            id char(32) PRIMARY KEY NOT NULL,
-            atmospheric_temp TEXT,
-            reservoir_temp TEXT,
-            light_intensity TEXT,
-            soil_moisture TEXT,
-            electrical_conductivity TEXT,
-            ph TEXT,
-            dissolved_oxygen TEXT,
-            air_flow TEXT,
-            foliage_feed TEXT,
-            root_stream TEXT,
-            CHECK(length(id) == 32 and TYPEOF(id) == 'text')
-        );
-    ''')
-    conn.executescript('''
-        CREATE TABLE if not exists owners (
-            monitor_id char(32) PRIMARY KEY NOT NULL,
-            user_id char(32) NOT NULL,
-            CHECK(length(user_id) == 32 and TYPEOF(user_id) == 'text' and length(monitor_id) == 32 and TYPEOF(monitor_id) == 'text')
-        );
-    ''')
-    cur = conn.cursor()
-    cur.execute("PRAGMA table_info(monitors)")
-    global table_typings
-    table_typings = [x[1] for x in cur.fetchall()]
-
+  conn.executescript('''
+    CREATE TABLE if not exists monitors (
+      id char(32) PRIMARY KEY NOT NULL,
+      atmospheric_temp TEXT,
+      reservoir_temp TEXT,
+      light_intensity TEXT,
+      soil_moisture TEXT,
+      electrical_conductivity TEXT,
+      ph TEXT,
+      dissolved_oxygen TEXT,
+      air_flow TEXT,
+      foliage_feed TEXT,
+      root_stream TEXT,
+      CHECK(length(id) == 32 and TYPEOF(id) == 'text')
+    );
+  ''')
+  conn.executescript('''
+    CREATE TABLE if not exists owners (
+      monitor_id char(32) PRIMARY KEY NOT NULL,
+      user_id char(32) NOT NULL,
+      CHECK(length(user_id) == 32 and TYPEOF(user_id) == 'text' and length(monitor_id) == 32 and TYPEOF(monitor_id) == 'text')
+    );
+  ''')
+  cur = conn.cursor()
+  cur.execute("PRAGMA table_info(monitors)")
+  global table_typings
+  table_typings = [x[1] for x in cur.fetchall()]
 
 def insert(conn, val):
-    cur = conn.cursor()
-    cur.execute("INSERT INTO monitors VALUES (?, ?, ?, ?)", val)
-    conn.commit()
+  cur = conn.cursor()
+  cur.execute("INSERT INTO monitors VALUES (?, ?, ?, ?)", val)
+  conn.commit()
 
 
 def isolate_updatable(content):
-    return {i[0]: i[1] for i in content.items(
-    ) if i[0] in table_typings and i[1] is not None}
-
+  return {i[0]: i[1] for i in content.items(
+  ) if i[0] in table_typings and i[1] is not None}
 
 def update_monitor(conn, val):
-    s = current_milli_time()
-    j = get_monitors(conn, val["id"])
-    l = []
-    for i in j.items():
-        if i[0] in val and i[0] != "id":
-            if isinstance(i[1], dict) and isinstance(
-                    val[i[0]], int) or isinstance(val[i[0]], float):
-                try:
-                    i[1]["value"] = float(val[i[0]])
-                    i[1]["history"][str(s)] = float(val[i[0]])
-                    l.append((i[0], json.dumps(i[1])))
-                except BaseException:
-                    pass
-            if isinstance(i[1], str):
-                l.append((i[0], val[i[0]]))
-    if len(l) == 0:
-        return {"success": False, "cause": "Invalid data"}, 400
-    cur = conn.cursor()
-    sql = "UPDATE monitors SET "
-    data = []
-    for i in l:
-        if i[0] != "id":
-            sql += i[0] + " = ?,"
-            data.append(i[1])
-    sql = sql[:-1] + " WHERE id = ?"
-    data.append(val["id"])
-    try:
-        cur.execute(sql, tuple(data))
-        conn.commit()
-    except BaseException:
-        return {"success": False, "cause": "Invalid data"}, 400
-
+  s = current_milli_time()
+  j = get_monitors(conn, val["id"])
+  l = []
+  for i in j.items():
+    if i[0] in val and i[0] != "id":
+      if isinstance(i[1], dict) and isinstance(val[i[0]], int) or isinstance(val[i[0]], float):
+        try:
+          i[1]["value"] = float(val[i[0]])
+          i[1]["history"][str(s)] = float(val[i[0]])
+          l.append((i[0], json.dumps(i[1])))
+        except BaseException:
+          pass
+      if isinstance(i[1], str):
+        l.append((i[0], val[i[0]]))
+  if len(l) == 0:
+    return {"success": False, "cause": "Invalid data"}, 400
+  cur = conn.cursor()
+  sql = "UPDATE monitors SET "
+  data = []
+  for i in l:
+    if i[0] != "id":
+      sql += i[0] + " = ?,"
+      data.append(i[1])
+  sql = sql[:-1] + " WHERE id = ?"
+  data.append(val["id"])
+  try:
+    cur.execute(sql, tuple(data))
+    conn.commit()
+  except BaseException:
+    return {"success": False, "cause": "Invalid data"}, 400
 
 def to_string(conn):
-    return (pd.read_sql_query("SELECT * FROM monitors", conn))
+  return (pd.read_sql_query("SELECT * FROM monitors", conn))
 
 def sqltojson(s):
-    json = {}
-    for a in s:
-        a = list(a)
-        k = a.pop(0)
-        t = ({table_typings[i + 1]: optionaljson(a[i])
-             for i in range(len(table_typings) - 1)})
-        if len(s) == 1:
-            return t
-        else:
-            json[k] = t
-    return json
+  json = {}
+  for a in s:
+    a = list(a)
+    k = a.pop(0)
+    t = ({table_typings[i + 1]: optionaljson(a[i])
+      for i in range(len(table_typings) - 1)})
+    if len(s) == 1:
+      return t
+    else:
+      json[k] = t
+  return json
 
 def get_monitors(conn, key=None):
-    cur = conn.cursor()
-    if key is not None:
-        cur.execute("SELECT * FROM monitors WHERE id = ?", (key,))
-    else:
-        cur.execute("SELECT * FROM monitors")
-    v = cur.fetchall()
-    return sqltojson(v)
+  cur = conn.cursor()
+  if key is not None:
+    cur.execute("SELECT * FROM monitors WHERE id = ?", (key,))
+  else:
+    cur.execute("SELECT * FROM monitors")
+  v = cur.fetchall()
+  return sqltojson(v)
 
 
 def get_owners(conn, key=None):
-    cur = conn.cursor()
-    if key is not None:
-        cur.execute(
-            "SELECT monitors.id FROM owners JOIN monitors ON monitors.id = owners.monitor_id WHERE user_id = ?",
-            (key,
-             ))
-        v = cur.fetchall()
-        return [x[0] for x in v]
-    else:
-        cur.execute("SELECT * FROM owners")
-        v = cur.fetchall()
-        s = {}
-        for a in v:
-            if a[1] not in s:
-                s[a[1]] = []
-            s[a[1]].append(a[0])
-        return s
+  cur = conn.cursor()
+  if key is not None:
+    cur.execute(
+        "SELECT monitors.id FROM owners JOIN monitors ON monitors.id = owners.monitor_id WHERE user_id = ?",
+        (key,
+          ))
+    v = cur.fetchall()
+    return [x[0] for x in v]
+  else:
+    cur.execute("SELECT * FROM owners")
+    v = cur.fetchall()
+    s = {}
+    for a in v:
+        if a[1] not in s:
+            s[a[1]] = []
+        s[a[1]].append(a[0])
+    return s
 
 
 def add_monitor(conn, val):
-    cur = conn.cursor()
-    try:
-        cur.execute("INSERT INTO owners VALUES (?, ?)", val)
-        cur.execute("""INSERT INTO monitors VALUES (?,
-            '{"value":null,"history":{}}',
-            '{"value":null,"history":{}}',
-            '{"value":null,"history":{}}',
-            '{"value":null,"history":{}}',
-            '{"value":null,"history":{}}',
-            '{"value":null,"history":{}}',
-            '{"value":null,"history":{}}',
-            '{"value":null,"history":{}}',
-            '',
-            ''
-        )
-        """, (val[0],))
-        conn.commit()
-    except Exception as e:
-        if str(e).startswith("CHECK constraint failed"):
-            return {"success": False, "cause": "Invalid id"}, 400
-        if str(e).startswith("UNIQUE constraint failed"):
-            return {"success": False, "cause": "monitor_id already exists"}, 400
-        return {"success": False, "cause": "Unknown Error"}, 400
+  cur = conn.cursor()
+  try:
+    cur.execute("INSERT INTO owners VALUES (?, ?)", val)
+    cur.execute("""INSERT INTO monitors VALUES (?,
+        '{"value":null,"history":{}}',
+        '{"value":null,"history":{}}',
+        '{"value":null,"history":{}}',
+        '{"value":null,"history":{}}',
+        '{"value":null,"history":{}}',
+        '{"value":null,"history":{}}',
+        '{"value":null,"history":{}}',
+        '{"value":null,"history":{}}',
+        '',
+        ''
+    )
+    """, (val[0],))
+    conn.commit()
+  except Exception as e:
+    if str(e).startswith("CHECK constraint failed"):
+      return {"success": False, "cause": "Invalid id"}, 400
+    if str(e).startswith("UNIQUE constraint failed"):
+      return {"success": False, "cause": "monitor_id already exists"}, 400
+    return {"success": False, "cause": "Unknown Error"}, 400
 
 
 def reset_monitor(conn, id):
     if len(get_monitors(conn, id)) == 0:
-        return {"success": False, "cause": "Invalid id"}, 400
+      return {"success": False, "cause": "Invalid id"}, 400
     cur = conn.cursor()
     try:
-        cur.execute("""UPDATE monitors SET
-            atmospheric_temp = '{"value":null,"history":{}}',
-            reservoir_temp = '{"value":null,"history":{}}',
-            light_intensity = '{"value":null,"history":{}}',
-            soil_moisture = '{"value":null,"history":{}}',
-            electrical_conductivity = '{"value":null,"history":{}}',
-            ph = '{"value":null,"history":{}}',
-            dissolved_oxygen = '{"value":null,"history":{}}',
-            air_flow = '{"value":null,"history":{}}',
-            foliage_feed = '',
-            root_stream = ''
-            WHERE id = ?
-        """, (id,))
-        conn.commit()
+      cur.execute("""UPDATE monitors SET
+          atmospheric_temp = '{"value":null,"history":{}}',
+          reservoir_temp = '{"value":null,"history":{}}',
+          light_intensity = '{"value":null,"history":{}}',
+          soil_moisture = '{"value":null,"history":{}}',
+          electrical_conductivity = '{"value":null,"history":{}}',
+          ph = '{"value":null,"history":{}}',
+          dissolved_oxygen = '{"value":null,"history":{}}',
+          air_flow = '{"value":null,"history":{}}',
+          foliage_feed = '',
+          root_stream = ''
+          WHERE id = ?
+      """, (id,))
+      conn.commit()
     except Exception as e:
         if str(e).startswith("CHECK constraint failed"):
             return {"success": False, "cause": "Invalid id"}, 400
@@ -190,20 +185,20 @@ def reset_monitor(conn, id):
 
 
 def delete_monitor(conn, id):
-    if len(get_monitors(conn, id)) == 0:
-        return {"success": False, "cause": "Invalid id"}, 400
-    cur = conn.cursor()
-    try:
-        cur.execute("DELETE FROM owners WHERE monitor_id = ?", (id,))
-        cur.execute("DELETE FROM monitors WHERE id = ?", (id,))
-        conn.commit()
-    except Exception as e:
-        print(e)
-        return {"success": False, "cause": "Unknown Error"}, 400
+  if len(get_monitors(conn, id)) == 0:
+      return {"success": False, "cause": "Invalid id"}, 400
+  cur = conn.cursor()
+  try:
+    cur.execute("DELETE FROM owners WHERE monitor_id = ?", (id,))
+    cur.execute("DELETE FROM monitors WHERE id = ?", (id,))
+    conn.commit()
+  except Exception as e:
+    print(e)
+    return {"success": False, "cause": "Unknown Error"}, 400
 
 
 def optionaljson(v):
-    try:
-        return json.loads(v)
-    except BaseException:
-        return v
+  try:
+    return json.loads(v)
+  except BaseException:
+    return v
